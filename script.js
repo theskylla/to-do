3a5c3a0776fcc5e51aef032ff9d36014ac531b83
theskylla <theskylla@gmail.com>
2025-09-17 15:54:40 +0200

    add to list + capital letter

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ var tasks = [];
 
     function addTask() { 
     const inputText = document.querySelector("input");
-    const task = inputText.value.trim();
+
+//capitalize first letter of task
+    const task = inputText.value.charAt(0).toUpperCase() + inputText.value.slice(1).toLocaleLowerCase();
+//clear input field
     const clearInput = document.querySelector("input").value = "";
+
+//select ul and create li element
+    const taskList = document.querySelector("ul");
+    const listItem = document.createElement("li");
+    const itemLabel = document.createElement("span");
 
 //if input is filled add task to array, if not, display error messagex
     if (task) {    
-   
+       listItem.innerText = task;
+       taskList.appendChild(listItem);
+       
        console.log("added " + task);
        tasks.push(task);
 
