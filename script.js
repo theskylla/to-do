8a539287ba43b55cb8dc16624e30641e337caa5b
theskylla <theskylla@gmail.com>
2025-09-17 10:22:37 +0200

    styling + start of script

--- script.js
+++ script.js
@@ -3,8 +3,24 @@
 var tasks = [];
 
     function addTask() { 
-      
+    const inputText = document.querySelector("input");
+    const task = inputText.value.trim();
+    const clearInput = document.querySelector("input").value = "";
 
-    }
-
+//if input is filled add task to array, if not, display error messagex
+    if (task) {    
    
+       console.log("added " + task);
+       tasks.push(task);
+
+//check tasks array       
+       console.log(tasks);
+       
+   
+      } else {
+
+  alert("Please enter something ");
+  console.log("no task added");  
+   
+}
+}
