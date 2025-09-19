d24218a968407b1de3fc2e9e1fb961827bec519d
theskylla <theskylla@gmail.com>
2025-09-19 11:07:06 +0200

    Replaced alert for error message + start of deleting list item

--- script.js
+++ script.js
@@ -1,49 +1,42 @@
-const { useCallback } = require("react");
 
 //array to hold tasks
 var tasks = [];
 
-    function addTask() { 
+    function addTask() {
     const inputText = document.querySelector("input");
 
-//capitalize first letter of task
+//capitalize first letter of task, clear input
     const task = inputText.value.charAt(0).toUpperCase() + inputText.value.slice(1).toLocaleLowerCase();
-//clear input field
     const clearInput = document.querySelector("input").value = "";
 
+//**lägg till kod som begränsar input till max __ tecken, QA-boyfriend tip, +felmeddelande det är för långt?
 
-//*lägg till kod som begränsar input till max __ tecken, QA-boyfriend tip*
-
-
-//select ul and create li element
-    const taskList = document.querySelector("ul");
-    const listItem = document.createElement("li");
-    const itemLabel = document.createElement("span");
-
-//if input is filled add task to array, if not, display error messagex
-    if (task) {    
-       listItem.innerText = task;
-       taskList.appendChild(listItem); 
-       itemLabel.className = "material-symbols-outlined";
-       itemLabel.innerText = "check_box_outline_blank";
-       itemLabel.setAttribute("onclick", "toggleBox(this)");
-       listItem.prepend(itemLabel); 
-     
-       
-       console.log("added " + task);
-       tasks.push(task);
-
-//check tasks array       
-       console.log(tasks);
-       
-      } else {
-
-  alert("Please enter something ");
-  console.log("no task added");     
+// Create a "close" button and add it to each list item
+var myTaskList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myTaskList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("              \u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myTaskList[i].appendChild(span);
 }
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
 
 //function to toggle between checked and unchecked box
+
+//**fixa så både checkbox och kryss använder samma metod */
+
+
 function toggleBox(el) {
   if (el.textContent.trim() === "check_box_outline_blank") {
     el.textContent = "check_box";
@@ -70,3 +63,45 @@ function toggleBox(el) {
     console.log("task uncompleted");
   }
 }
+
+//select ul and create li element
+    const taskList = document.querySelector("ul");
+    const listItem = document.createElement("li");
+    const itemLabel = document.createElement("span");
+
+//if input is filled add task to array, if not, display error messagex
+    if (task) {    
+       taskList.appendChild(listItem); 
+       listItem.innerText = task;
+       itemLabel.className = "material-symbols-outlined";
+       itemLabel.innerText = "check_box_outline_blank";
+       itemLabel.setAttribute("onclick", "toggleBox(this)");
+       listItem.prepend(itemLabel); 
+     
+       
+       console.log("added " + task);
+       tasks.push(task);
+
+//check tasks array       
+       console.log(tasks);
+       
+
+    } else {
+
+  //display error message if input is empty
+  const messageBox = document.querySelector("ul");
+  const errorMessage = document.createElement("span");
+  messageBox.appendChild(errorMessage);
+  errorMessage.style.color = "red";
+    errorMessage.innerText = " *Please enter a task";
+
+  //remove error messaage with click on input field
+  inputText.onclick = function() {
+  errorMessage.remove();
+    
+ //say if task not added
+  console.log("no task added");     
+}
+}
+    }
+
